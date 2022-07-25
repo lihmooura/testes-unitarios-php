5053540ef39f9502609d2001bd1152b43f70553d
Elise Melo <elisemelo@Elises-MacBook-Air.local>
2022-07-25 10:42:22 -0300

    update

--- teste-avaliador.php
+++ teste-avaliador.php
@@ -7,7 +7,7 @@ use Alura\Leilao\Service\Avaliador;
 
 require 'vendor/autoload.php';
 
-// Arrange - Given : usado para demontrar informações para o teste
+// Arrange - Given
 $leilao = new Leilao('Fiat 147 0KM');
 
 $maria = new Usuario('Maria');
@@ -18,12 +18,12 @@ $leilao->recebeLance(new Lance($maria, 2500));
 
 $leiloeiro = new Avaliador();
 
-// Act - When : usado para demonstrar o que testar
+// Act - When
 $leiloeiro->avalia($leilao);
 
 $maiorValor = $leiloeiro->getMaiorValor();
 
-// Assert - Then : usado para quando o teste for realizado  mostrar o resultado
+// Assert - Then
 $valorEsperado = 2500;
 
 if ($valorEsperado == $maiorValor) {
@@ -31,3 +31,4 @@ if ($valorEsperado == $maiorValor) {
 } else {
     echo "TESTE FALHOU";
 }
+
